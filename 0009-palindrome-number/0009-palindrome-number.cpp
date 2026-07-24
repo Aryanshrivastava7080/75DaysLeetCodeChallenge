c9ada42e3f2c0f74e658d22855fb66c85e1ce0b9
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-07-24 23:44:45 +0530

    Time: 3 ms (44.46%), Space: 8.7 MB (37.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long ans=0;
+        int target=x;
+        
+        if(target<0) return false;
+        while(x!=0){
+            int digit = x%10;
+            ans = ans*10+digit;
+            x=x/10;
+        }
+        if(ans==target) return true;
+        return false;
+
+    }
+};
