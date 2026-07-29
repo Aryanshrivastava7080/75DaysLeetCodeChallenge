1432162786f70fd450e67e2b6a0d24bd0cf4cea5
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-07-29 13:20:08 +0530

    Time: 7 ms (10.49%), Space: 12.2 MB (97.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> mp;
-        stack<int> st;
 
-        // Process nums2 from right to left
-        for (int i = nums2.size() - 1; i >= 0; i--) {
+        vector<int>ans;
 
-            while (!st.empty() && st.top() <= nums2[i]) {
-                st.pop();
+        for(int i=0; i<nums1.size(); i++){
+
+            int j=0;
+
+            //now search in nums2 position 
+
+            while(nums1[i]!=nums2[j]){
+                j++;
             }
 
-            if (st.empty())
-                mp[nums2[i]] = -1;
-            else
-                mp[nums2[i]] = st.top();
+            int nxtgreater= -1;
 
-            st.push(nums2[i]);
+            //right side traverse
+
+            for(int k=j+1; k<nums2.size(); k++){
+
+                if(nums2[k]>nums2[j]){
+                    nxtgreater = nums2[k];
+                    break;
+                }
+
+            }
+            ans.push_back(nxtgreater);
         }
-
-        vector<int> ans;
-
-        for (int num : nums1) {
-            ans.push_back(mp[num]);
-        }
-
         return ans;
     }
 };
+//         unordered_map<int, int> mp;
+//         stack<int> st;
+
+//         // Process nums2 from right to left
+//         for (int i = nums2.size() - 1; i >= 0; i--) {
+
+//             while (!st.empty() && st.top() <= nums2[i]) {
+//                 st.pop();
+//             }
+
+//             if (st.empty())
+//                 mp[nums2[i]] = -1;
+//             else
+//                 mp[nums2[i]] = st.top();
+
+//             st.push(nums2[i]);
+//         }
+
+//         vector<int> ans;
+
+//         for (int num : nums1) {
+//             ans.push_back(mp[num]);
+//         }
+
+//         return ans;
+//     }
+// };
