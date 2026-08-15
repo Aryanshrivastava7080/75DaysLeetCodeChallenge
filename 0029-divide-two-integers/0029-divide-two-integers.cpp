79b6a1c8a310a5929cc5c0d380d034fb829b1984
Aryan Srivastava <145749664+Aryanshrivastava7080@users.noreply.github.com>
2026-08-15 10:31:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (74.01%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,33 +1,67 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-       if (dividend == INT_MIN && divisor == -1)
+        if(dividend == INT_MIN && divisor == -1){
             return INT_MAX;
+        }
 
-        bool neg = (dividend < 0) ^ (divisor < 0);
+        //CHECK NEGATIVE POSITIVE
+        bool check = (dividend < 0)^(divisor < 0);
 
         long long a = abs((long long)dividend);
         long long b = abs((long long)divisor);
 
-        long long ans = 0;
+        //initialize ans
+        long long ans=0;
 
-        while (a >= b) {
+        //iteration
+        while(a>=b){
+            long long temp=b;
+            long long count=1;
 
-            long long temp = b;
-            long long count = 1;
-
-            while (a >= temp + temp) {
-                temp += temp;
-                count += count;
+            while(a>=temp+temp){
+                temp+=temp;
+                count+=count;
             }
-
-            a -= temp;
-            ans += count;
+            a-=temp;
+            ans+=count;
         }
-
-        if (neg)
-            ans = -ans;
-
+        if(check){
+            ans=-ans;
+        }else{
+            return ans;
+        }
         return ans;
+
     }
 };
+//        if (dividend == INT_MIN && divisor == -1)
+//             return INT_MAX;
+
+//         bool neg = (dividend < 0) ^ (divisor < 0);
+
+//         long long a = abs((long long)dividend);
+//         long long b = abs((long long)divisor);
+
+//         long long ans = 0;
+
+//         while (a >= b) {
+
+//             long long temp = b;
+//             long long count = 1;
+
+//             while (a >= temp + temp) {
+//                 temp += temp;
+//                 count += count;
+//             }
+
+//             a -= temp;
+//             ans += count;
+//         }
+
+//         if (neg)
+//             ans = -ans;
+
+//         return ans;
+//     }
+// };
